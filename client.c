9f7b3e80a43be45162231624ac0cb101880e911d
merc76 <78970299+merc76@users.noreply.github.com>
2022-12-20 07:37:10 +0100

    ajout préparation pre partie

--- client.c
+++ client.c
@@ -13,25 +13,66 @@
 #include <signal.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <string.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <pthread.h>
+#include "time.h"
 #include "GuitarGhetto.h"
+
+/*******************************************************************************************************/
+//constantes
+
+//TODO: pour une prochaine versions on pourrait avec un temps différents entre chaque notes
+//TODO temps qu'on pourrait observer dans les partitions
+#define TEMPS_PAR_NOTE 350 //en millisecondes
+#define DELTA_T_PERFECT 100 //ms
+#define DELTA_T_GOOD 200 //ms
+#define DELTA_T_BAD 300 //msg
+
+#define SCORE_PERFECT 500
+#define SCORE_GOOD 200
+#define SCORE_BAD 50
+#define SCORE_WORSE -10
+
+/*******************************************************************************************************/
+//empreintes fonctions
 
 void CHECK(int code, char * toprint);
 void clientDeroute(int, siginfo_t*, void*);
+void *routineAffichage(void *);
+void *routineEnvoieScore(void *);
+void *routineReceptionScore(void *);
 
+/*******************************************************************************************************/
+// variables globales
+
+char partitionEnCours[TAILLE_PARTITION];
+int monScore;
+preGameLetter_t preGame;
+pid_t pidServeur;
+/*******************************************************************************************************/
+
+/**
+ * @brief 
+ * 
+ * @return int 
+ */
 int main(){
     struct sigaction sa;
     sa.sa_flags = SA_SIGINFO;
     sa.sa_sigaction = clientDeroute;
-    pid_t pidServeur;
+    key_t cleServeur;
+    int msqidServeur;
+    partitionLettre_t partitionAJouer;
     char tmp[20];
+    pthread_t threadAffichage, threadEnvoieScore, threadReceptionScore;
+    time_t debutNote, tempsReaction, finNote;
 
     printf("bienvenue à vous !!\n merci d'entrer votre code de partie !\n");
-    fgets(tmp, 5, stdin);
+    fgets(tmp, 6, stdin);
     pidServeur = atoi(tmp);
-    kill(pidServeur, SIGUSR1);
-
 
     //Clear les signaux
     CHECK(sigemptyset(&sa.sa_mask), "[Child] Erreur sigemptyset\n");
@@ -43,17 +84,57 @@ int main(){
     CHECK(sigaction(SIGUSR2, &sa, NULL), "Erreur client ne peut pas ajouter SIGUSR2");
     CHECK(sigaction(SIGALRM, &sa, NULL), "Erreur client ne peut pas ajouter SIGALRM");
 
-    while(1){}
+    //préparation avant la fin de partie
+    kill(pidServeur, SIGUSR1);
 
+    //on tampone pour laisser le temps au serveur de créer la BAL
+    sleep(1);
+    cleServeur = ftok(FIC_BAL, getpid());
+    msqidServeur = msgget(cleServeur, 0666);
+    msgrcv(msqidServeur, &partitionAJouer, sizeof(partitionLettre_t), MTYPE_ENVOI_PARTITION, 0664);
+    strcpy(partitionEnCours, partitionListe[partitionAJouer.content]);
+    printf("client n°%d, la partition n°%d à été chargée\n", getpid(), partitionAJouer.content);
+
+    //on créer les 3 autres threads
+    pthread_create(&threadAffichage, NULL, routineAffichage, NULL);
+    pthread_create(&threadEnvoieScore, NULL, routineEnvoieScore, NULL);
+    pthread_create(&threadReceptionScore, NULL, routineReceptionScore, NULL);
+    
+    do{
+        msgrcv(msqidServeur, &preGame, sizeof(preGameLetter_t), MTYPE_PRE_PARTIE | MTYPE_DEBUT_PARTIE, 0664);
+        alarm(1);
+    }while(preGame.mtype != MTYPE_DEBUT_PARTIE);
+    //fin de préparation début de partie
+
+    //partie en cours
+    while(partitionEnCours[0] != '\0'){
+        
+    }
+
+    //préparation fin de partie
+    pthread_join(threadAffichage, NULL);
+    pthread_join(threadEnvoieScore, NULL);
+    pthread_join(threadReceptionScore, NULL);
+    while (1)
+    {
+    }
+   
     return 0;
 }
 
+/**
+ * @brief permet la reception des signaux et de leurs affecter une réponse adéquat 
+ * 
+ * @param sig le signal reçu
+ * @param sa les informations relative au signal reçu
+ * @param context 
+ */
 void clientDeroute(int sig, siginfo_t *sa, void *context){
     switch (sig) {
     case SIGALRM:
-        //printf("[Parent] Timeout\n");
-        // On arrete le processus fils
-        //kill(pidChild, SIGTERM);
+        printf("client n°%d, temps avant début de partie : %d\n", getpid(), preGame.content.secondeRestant);
+        printf("client n°%d, joueurs dans le hub : %d\n", getpid(), preGame.content.nbJoueur);
+
         break;
 
     case SIGUSR1:
@@ -69,9 +150,47 @@ void clientDeroute(int sig, siginfo_t *sa, void *context){
     }
 }
 
+/**
+ * @brief vérifie si le retour d'une fonction est un succès ou échec
+ *      dans le cas d'un échec renvoie la phrase toprint
+ * 
+ * @param code le code de retour
+ * @param toprint la phrase à affiché en cas d'échec
+ */
 void CHECK(int code, char * toprint){
     if(code < 0){
         printf("%s \n", toprint);
         exit(EXIT_FAILURE);
     }
+}
+
+/**
+ * @brief permet d'envoyer son score personnel toutes les x secondes
+ * 
+ * @param noth nothing
+ */
+void *routineEnvoieScore(void* noth){
+    //TODO
+    pthread_exit;
+}
+
+/**
+ * @brief permet la reception des scores des autres joueurs en boucles
+ * 
+ * @param noth nothing
+ */
+void *routineReceptionScore(void * noth){
+    //TODO
+    pthread_exit;
+}
+
+/**
+ * @brief affiche une interface plus agréable pour le joueur
+ * 
+ * @param noth 
+ * @return void* 
+ */
+void *routineAffichage(void * noth){
+    //TODO
+    pthread_exit;
 }
